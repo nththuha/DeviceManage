cbecf9c89eb7b3ee10f142831429a2546ce9538e
Ngô Thu Hà <n18dccn052@student.ptithcm.edu.vn>
2022-04-21 00:22:04 +0700

    update trước khi đi ngủ :))

--- app/src/main/java/com/example/devicemanagement/Controller/LoaiThietBiActivity.java
+++ app/src/main/java/com/example/devicemanagement/Controller/LoaiThietBiActivity.java
@@ -3,14 +3,43 @@ package com.example.devicemanagement.Controller;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.ImageButton;
+import android.widget.ListView;
+import android.widget.SearchView;
 
+import com.example.devicemanagement.Adapter.AdapterLoaiThietBi;
+import com.example.devicemanagement.DBHelper.DBLoaiThietBi;
+import com.example.devicemanagement.Entity.LoaiThietBi;
 import com.example.devicemanagement.R;
 
+import java.util.ArrayList;
+
 public class LoaiThietBiActivity extends AppCompatActivity {
+    ListView lvLTB;
+    ImageButton imbThemLTB;
+    SearchView svLTB;
+    AdapterLoaiThietBi adapterLoaiThietBi;
+    ArrayList<LoaiThietBi> DSLTB;
+    DBLoaiThietBi dbLoaiThietBi;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_loai_thiet_bi);
+        setControl();
+        setEvent();
+    }
+
+    private void setEvent() {
+        dbLoaiThietBi = new DBLoaiThietBi(this);
+        DSLTB = dbLoaiThietBi.layDanhSachLTB();
+        adapterLoaiThietBi = new AdapterLoaiThietBi(this, R.layout.layout_loai_thiet_bi, DSLTB);
+        lvLTB.setAdapter(adapterLoaiThietBi);
+    }
+
+    private void setControl() {
+        lvLTB = findViewById(R.id.lvLTB);
+        imbThemLTB = findViewById(R.id.imbThemLTB);
+        svLTB = findViewById(R.id.svLTB);
     }
 }
